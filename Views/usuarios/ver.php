f3e3b8da40d0a4693efa6acbf23fa7b5b9d93d6e
Mikhail Piataev <mikhail.piataev@davinci.edu.ar>
2024-11-07 14:35:08 -0300

    Update ver.php

--- Views/usuarios/ver.php
+++ Views/usuarios/ver.php
@@ -1,5 +1,5 @@
 <div class="box-principal">
-<h3 class="titulo">Usuario <?php echo $datos['id_usuario']; ?><hr></h3>
+<h3 class="titulo">Usuario: <?php echo $datos['nombre']. " " . $datos['apellido']; ?><hr></h3>
   <div class="panel panel-success">
     <div class="panel-heading">
       <h3 class="panel-title">Perfil del usuario <?php echo $datos['id_usuario']; ?><b></b></h3>
@@ -13,9 +13,6 @@
             </li>
             <li class="list-group-item">
               <b>E-mail: </b><?php echo $datos['email']; ?>
-            </li>
-            <li class="list-group-item">
-              <b>Password: </b><?php echo $datos['password']; ?>
             </li>
             <li class="list-group-item">
               <b>Role: </b><?php echo $datos['role_usuario']; ?>
