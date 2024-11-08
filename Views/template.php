93422399d0c1933dcd9d4e3c8be40ea52eef3a41
udam1305 <adamarmoza@gmail.com>
2024-11-07 23:35:55 -0300

    pepe

--- Views/template.php
+++ Views/template.php
@@ -15,48 +15,50 @@
 		<link rel="stylesheet" href="<?php echo URL; ?>Views/_template/css/general.css">
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-  		<div class="container-fluid">
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
-		        <span class="sr-only"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#">Administración de Productos</a>
-		    </div>
+	<nav class="navbar navbar-custom navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarContent">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Administración de Productos</a>
+			</div>
 
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
-		      <ul class="nav navbar-nav">
-		        <li><a href="<?php echo URL; ?>">Inicio</a></li>
-						<li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Productos <span class="caret"></span></a>
-		          <ul class="dropdown-menu" role="menu">
-		            <li><a href="<?php echo URL; ?>productos">Ver productos</a></li>
-		            <li><a href="<?php echo URL; ?>productos/agregar">Agregar producto</a></li>
-		          </ul>
-		        </li>
-						<li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Usuarios <span class="caret"></span></a>
-		          <ul class="dropdown-menu" role="menu">
-		            <li><a href="<?php echo URL; ?>usuarios">Ver ususarios</a></li>
-		            <li><a href="<?php echo URL; ?>usuarios/agregar">Agregar usuario</a></li>
-		          </ul>
-		        </li>
-				<li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Pagos <span class="caret"></span></a>
-		          <ul class="dropdown-menu" role="menu">
-		            <li><a href="<?php echo URL; ?>pagos">Ver pagos</a></li>
-		            <li><a href="<?php echo URL; ?>pagos/agregar">Agregar pago</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		      
-		      <ul class="nav navbar-nav navbar-right">
-		       </div>
-		  </div>
-		</nav>
+			<div class="collapse navbar-collapse" id="navbarContent">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo URL; ?>">Inicio</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Productos <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="<?php echo URL; ?>productos">Ver productos</a></li>
+							<li><a href="<?php echo URL; ?>productos/agregar">Agregar producto</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Usuarios <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="<?php echo URL; ?>usuarios">Ver usuarios</a></li>
+							<li><a href="<?php echo URL; ?>usuarios/agregar">Agregar usuario</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Pagos <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="<?php echo URL; ?>pagos">Ver pagos</a></li>
+							<li><a href="<?php echo URL; ?>pagos/agregar">Agregar pago</a></li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?php echo URL; ?>logout">Cerrar sesión</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
 <?php
 		}
 
